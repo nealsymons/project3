a89c622eb9985409630e8ab7ffe083208c200fbb
NealSymons <nealsymons4@gmail.com>
2023-03-23 16:05:43 -0400

    Thursday Mar 23 setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
 }
 
 export default App;
+
+// Create state items for the API data and the user selection from the list
+  // pokemon
+  // userSelection
+
+// Once the component has been mounted, call the local method to get the data from the Pokemon API
+
+// A local method getPokemon to make the API call for the selected Pokemon from the list
+  // when successful, update the state with the API data
+  // else display an error message
+
+// Render the app
+  // header
+  // drop down list of Pokemon
+  // footer
+
+// Use the return to render the Pokemon card 
+  // picture
+  // stats and other info
+
+// Return
+  // Create a/some component/s to display an image of the Pokemon along with other text information below it
+  // these components will get data passed in a props
+  // 
